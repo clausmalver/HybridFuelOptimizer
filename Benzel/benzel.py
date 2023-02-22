6c40ee26aae49bcb1d3b078f97de75e202be04a7
Lethfx <116078627+Lethfx@users.noreply.github.com>
2023-02-22 22:15:43 +0100

    Update benzel.py

--- Benzel/benzel.py
+++ Benzel/benzel.py
@@ -1,16 +1,20 @@
-KMperLiter = 13.5
-KMperkWh = 2.68
+# Mitsubishi Outlander 2020 PHEV
+# It drives around 13.5 km per liter gasoline
+# Average reach on the battery around 37 km on a full battery charge 13.8 kwh
 
-gasPrice = float(input("Gasoline price in DKK (kr/L): "))
-elPrice = float(input("Electricity price in DKK (kr/kWh): "))
+km_per_liter = 13.5
+km_per_kwh = 2.68
 
-pricePerKMgas = gasPrice/KMperLiter
-pricePerKMElectric = elPrice/KMperkWh
+gas_price = float(input("What is the current gasoline price in DKK (kr/l): "))
+el_price = float(input("What is the current electricity price in DKK (kr/kWh): "))
 
-print("\n-----------------------------\nBenzin: {:.2f} kr/km".format(pricePerKMgas))
-print("El: {:.2f} kr/km\n-----------------------------\n".format(pricePerKMElectric))
+gas_price_per_km = gas_price / km_per_liter
+el_price_per_km = el_price / km_per_kwh
 
-if pricePerKMElectric < pricePerKMgas:
-    print("It is cheaper to drive on electricity than gasoline. You should charge your electric vehicle tonight.")
-else:
-print("It is cheaper to drive on gasoline than electricity.")
+print(f"\n{'-'*30}\nGasoline: {gas_price_per_km:.2f} kr/km")
+print(f"Electricity: {el_price_per_km:.2f} kr/km\n{'-'*30}\n")
+
+cheaper_fuel_type = "electricity" if el_price_per_km < gas_price_per_km else "gasoline"
+expensive_fuel_type = "electricity" if el_price_per_km > gas_price_per_km else "gasoline"
+
+print(f"It is cheaper to drive on {cheaper_fuel_type} than {expensive_fuel_type}. You should {'charge your electric vehicle tonight.' if cheaper_fuel_type == 'electricity' else 'fill up your gasoline vehicle.'}")
